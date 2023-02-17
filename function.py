98d684168d7b6090db33c1ddf3c488fd1b0de41d
Solarev Ruslan <ruslan251207@gmail.com>
2023-02-17 21:42:45 +1000

    added players and counter

--- function.py
+++ function.py
@@ -1,12 +1,32 @@
 import random
 def guess_the_number(begin, end):
+    players = []
+    count_of_players = int(input('Выберите количество игроков:'))
+    while count_of_players < 1:
+        count_of_players = int(input('Выберите количество игроков:'))
+    
+    for n in range(1, count_of_players + 1):
+        player = input(f'Имя игрока {n}: ')
+        players.append(player)
+    
     number = random.randint(begin, end)
-    answer = int(input('Какое число загадал компьютер:'))
+    cur_pl_index = 0
+    current_player = players[cur_pl_index]
+    answer = int(input(f'{current_player}, какое число загадал компьютер: '))
+    count_of_attempts = 1
+
     while answer != number:
         if answer < number:
             print('Загаданное число больше')
         else:
             print('Загаданное число меньше')
         print('Переход хода!')
-        answer = int(input('Выберите новое число:'))
-    print(f'Вы отгадали число {number}')
+        cur_pl_index += 1
+        if cur_pl_index == len(players):
+            cur_pl_index = 0
+        current_player = players[cur_pl_index]
+        answer = int(input(f'{current_player}, выберите новое число: '))
+        count_of_attempts += 1
+
+    print(f'{current_player} отгадал(-a) число {number}')
+    print(f'Общее количество попыток: {count_of_attempts}')
